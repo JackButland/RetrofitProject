782e82aee6f19ee18b4588006b6a988e356d91c2
JackButland <jack_butland_msk@yahoo.com>
2019-05-12 22:32:33 +0300

    okhttp branch init

--- src/main/java/RetrofitMin.java
+++ src/main/java/RetrofitMin.java
@@ -4,59 +4,30 @@ import java.util.Iterator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import io.reactivex.Single;
-import io.reactivex.SingleObserver;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class RetrofitMin {
 
 	final static String API_KEY="2d4c222e7204d579867b4068c0094d67";
 	
 	public static void main(String[] args) {		
-		
-        RestApiInterface apiService =
-                APIClient.getClient().create(RestApiInterface.class);
- 
-        apiService.getWeatherForecast("524901",API_KEY)
-        	.subscribeWith(new SingleObserver<POJOresponse>() {
-            
-        		@Override
-        		public void onError(Throwable e) {
-        			e.printStackTrace();
-        		}
 
-        		@Override
-        		public void onSuccess(POJOresponse t) {
-        			System.out.println("success");
-        			System.out.println(t.getCity().getName());
-        		}
-
-				@Override
-				public void onSubscribe(Disposable d) {
-					System.out.println("subscription made");
-					
-				}
-        });
-
-		/*OkHttpClient client=new OkHttpClient();
+		OkHttpClient client=new OkHttpClient();
 		Request req=new Request.Builder()
 				.url("https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2d4c222e7204d579867b4068c0094d67")
 				.build();
 		Call call= client.newCall(req);
 		call.enqueue(new Callback() {
 
-			@Override
 			public void onFailure(Call call, IOException e) {
 				// TODO Auto-generated method stub
 				
 			}
 
-			@Override
 			public void onResponse(Call call, Response response) throws IOException {
 				try {
 					JSONObject obj=new JSONObject(response.body().string());
@@ -74,7 +45,7 @@ public class RetrofitMin {
 				}
 			}
 			
-		});*/
+		});
 	}
 
 }
